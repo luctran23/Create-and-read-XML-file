bb6f00a9a750715a8b538ccae4c79d262a46f0cf
mrTran <anhvanluc1999@gmail.com>
2020-04-09 19:34:45 +0700

    Create XML file using C#

---
+++ baitapthem/createXmlFile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+
+
+
+namespace bonusTask_bailamthem
+{
+    class createXmlFile
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement root = doc.CreateElement("thuvien");
+            doc.AppendChild(root);
+
+            Sach s1 = new Sach("nxb Giaoduc", "b01", "Lap trinh C#", "200");
+            themSach(doc, root, s1);
+        }
+        class Sach
+        {
+            public String nxb;
+            public String masach;
+            public String tensach;
+            public String giatien;
+            public Sach(String nxb, String masach, String tensach, String giatien)
+            {
+                this.nxb = nxb;
+                this.masach = masach;
+                this.tensach = tensach;
+                this.giatien = giatien;
+            }
+        }
+        static void themSach(XmlDocument doc, XmlElement root, Sach s)
+        {
+            XmlElement sach = doc.CreateElement("cuonsach");
+            root.AppendChild(sach);
+            sach.SetAttribute("nxb", s.nxb);
+
+            XmlElement masach = doc.CreateElement("masach");
+            masach.InnerText = s.masach;
+            sach.AppendChild(masach);
+
+            XmlElement tensach = doc.CreateElement("tensach");
+            tensach.InnerText = s.tensach;
+            sach.AppendChild(tensach);
+
+            XmlElement giatien = doc.CreateElement("giatien");
+            giatien.InnerText = s.giatien;
+            sach.AppendChild(giatien);
+
+            doc.Save(Console.Out);
+            doc.Save("thuvien.xml");
+        }
+    }
+}
---
+++ baitapthem/readXmlFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+
+namespace bonusTask_bailamthem
+{
+    class readXmlFile
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("thuvien.xml");
+            XmlElement root = doc.DocumentElement;
+            printLib(root);
+        }
+        static void printLib(XmlNode node)
+        {
+            Console.WriteLine("\nNode name: " + node.Name);
+            Console.WriteLine("Node type: " + node.NodeType);
+            Console.WriteLine("Node value: " + node.Value);
+            XmlNodeList children = node.ChildNodes;
+            if(children.Count > 0)
+            {
+                
+                for (int i = 0; i < children.Count; i++ )
+                {
+                    printLib(children.Item(i));
+                }
+                Console.WriteLine("\n-----KET THUC NODE " + node.Name + "-----------");
+            }
+        }
+    }
+}
